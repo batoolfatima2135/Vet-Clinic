45e0249d8ec471abe398dbb11363bffc81110a9e
Batool Fatima <batoolfatima2135@gmail.com>
2023-08-15 14:31:39 +0500

    Add new tables and modify animal table

--- schema.sql
+++ schema.sql
@@ -10,3 +10,13 @@ CREATE TABLE animals(
  );
 
 ALTER TABLE animals ADD species VARCHAR(250);
+
+CREATE TABLE owners(id INT GENERATED ALWAYS AS IDENTITY, full_name VARCHAR(250), age INT, PRIMARY KEY (id));
+CREATE TABLE species(id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(250), PRIMARY KEY (id));
+
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD PRIMARY KEY (id);
+ALTER TABLE animals ADD species_id INT REFERENCES species(id);
+ALTER TABLE animals ADD owner_id INT REFERENCES owners(id);
+
